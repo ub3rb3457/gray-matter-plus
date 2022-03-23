d3cb2cbdf5e798ff9ee095d514aa495c2c958a24
ub3rb3457 <31402226+ub3rb3457@users.noreply.github.com>
2022-03-23 10:54:52 -0400

    previously stashed changes

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,10 +9,30 @@ module.exports = {
     "@storybook/addon-interactions"
   ],
   "framework": "@storybook/vue3",
+  "features": {
+    "interactionsDebugger": true,
+  },
   "core": {
     "builder": "storybook-builder-vite"
   },
-  "features": {
-    "interactionsDebugger": true,
+  /**
+   * Extend Vite config
+   * @param {import('vite').UserConfig} config
+   * @param {'DEVELOPMENT'|'PRODUCTION'} [configType]
+   * @returns {Promise<*>}
+   */
+  async viteFinal(config, { configType }) {
+    // Needed only for development mode: `npm run storybook`
+    config.optimizeDeps = configType === 'PRODUCTION' ? config.optimizeDeps : {
+      ...(config.optimizeDeps || {}),
+      include: [
+        ...(config?.optimizeDeps?.include || []),
+        // Fix: `@storybook/addon-interactions` exports is not defined or `jest-mock` does not provide an export named 'fn'
+        'jest-mock'
+      ],
+    }
+    // ...
+    return config;
   }
+
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import * as jest from '@storybook/jest';
+window.jest = jest;
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
