5b37c94c0aaaae426f801181d0ea94fe6dce8f53
ub3rb3457 <31402226+ub3rb3457@users.noreply.github.com>
2022-03-24 03:23:54 -0400

    fine-tuning configuration options

--- quasar.config.js
+++ quasar.config.js
@@ -59,6 +59,9 @@ module.exports = configure(function (/* ctx */) {
         browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
         node: 'node16'
       },
+      alias: {
+        myalias: path.join(__dirname, './src/myalias')
+      },
 
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
@@ -133,8 +136,6 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
       // ssrPwaHtmlFilename: 'offline.html', // do NOT use index.html as name!
-                                          // will mess up SSR
-
       // extendSSRWebserverConf (esbuildConf) {},
       // extendPackageJson (json) {},
 
@@ -144,8 +145,6 @@ module.exports = configure(function (/* ctx */) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
-
       middlewares: [
         'render' // keep this as last one
       ]
