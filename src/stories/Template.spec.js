af4b7f2b4b7e1f36fbdcda05cfa2d8fee2c26ca2
ub3rb3457 <31402226+ub3rb3457@users.noreply.github.com>
2022-04-07 15:27:59 -0400

    ✅ TEST: testing setup

--- src/stories/Template.spec.js
+++ src/stories/Template.spec.js
@@ -2,16 +2,15 @@ import { render } from '@testing-library/vue';
 import { composeStories } from '@storybook/testing-vue3';
 import * as Stories from './Button.stories';
 
-const { Primary } = composeStories(Stories)
+const { Primary/*, Secondary, Tertiary */ } = composeStories(Stories)
 
 describe( 'Basic Functionality', () => {
-  test('test the story ', async () => {
-  const { container } = render(Primary());
+  test('Run the tests in the `.stories` file ', async () => {
+    const { container } = render(Primary());
     await Stories.Primary.play?.({ canvasElement: container });
+    // repeat for each story
   });
-
+  // if you need to leave a note to remind you of something when the tests run:
   test.todo('boom')
-
-  
 })
 
